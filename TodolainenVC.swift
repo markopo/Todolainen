4f35c5ae982619198d90ac97a21e2267aa64be9b
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-03-26 20:44:40 +0200

    1st commit

---
+++ TodolainenVC.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Todolainen
+//
+//  Created by Marko Poikkimäki on 2018-03-26.
+//  Copyright © 2018 Marko Poikkimäki. All rights reserved.
+//
+
+import UIKit
+
+class TodolainenVC: UITableViewController {
+    
+    let itemArray = ["Bajsa", "Äta mat", "Dricka öl"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+
+
+}
+
