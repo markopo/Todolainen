82cac081ac864c51641023f48998636e573c5445
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-04-16 07:29:11 +0200

    Resign

--- Controllers/TodolainenVC.swift
+++ Controllers/TodolainenVC.swift
@@ -132,6 +132,17 @@ extension TodolainenVC: UISearchBarDelegate {
         
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if(searchBar.text?.count == 0){
+            loadItems()
+            
+            DispatchQueue.main.async {
+                  searchBar.resignFirstResponder()
+            }
+          
+        }
+        
+    }
     
 }
 
